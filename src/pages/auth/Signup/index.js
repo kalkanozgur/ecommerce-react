5b7034ab7ac48ff56ddc9fb1a8d941737a3094c0
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-09-12 14:26:22 +0300

    Sing Up page created

--- src/pages/auth/Signup/index.js
+++ src/pages/auth/Signup/index.js
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+	Flex,
+	Box,
+	Heading,
+	FormControl,
+	FormLabel,
+	Input,
+	Button,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
 
 function Signup() {
-	return <div>Signup</div>;
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+			passwordConfirm: "",
+		},
+	});
+	return (
+		<div>
+			<Flex align={"center"} width={"full"} justifyContent={"center"}>
+				<Box pt={10}>
+					<Box textAlign={"center"}>
+						<Heading>Sign Up</Heading>
+					</Box>
+					<Box my={5} textAlign={"left"}>
+						<form action="" onSubmit={() => {}}>
+							<FormControl>
+								<FormLabel>E-mail</FormLabel>
+								<Input name="email" />
+							</FormControl>
+							<FormControl mt={4}>
+								<FormLabel>Password</FormLabel>
+								<Input name="password" type={"password"} />
+							</FormControl>
+							<FormControl mt={4}>
+								<FormLabel>Password Confirm</FormLabel>
+								<Input name="password" type={"password"} />
+							</FormControl>
+							<Button mt={4} width={"full"} type={"submit"}>
+								Sign Up
+							</Button>
+						</form>
+					</Box>
+				</Box>
+			</Flex>
+		</div>
+	);
 }
 
 export default Signup;
