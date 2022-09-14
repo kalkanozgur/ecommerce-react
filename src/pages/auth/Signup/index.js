1f3c422081f0188bf3ebf1118dddd60d169ef9c5
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-09-14 19:57:06 +0300

    Revert "fetchRegister && bag.setErrors"
    This reverts commit 8dbef81eb16ba4ece0c61f4ac18b71d1db6daa93.
    
    	modified:   src/api.js
    	modified:   src/pages/auth/Signup/index.js

--- src/pages/auth/Signup/index.js
+++ src/pages/auth/Signup/index.js
@@ -7,12 +7,10 @@ import {
 	FormLabel,
 	Input,
 	Button,
-	Alert,
 } from "@chakra-ui/react";
 import { useFormik } from "formik";
 
-import validationSchema from "./validations";
-import { fetchRegister } from "./../../../api";
+import validationScheme from "./validations";
 
 function Signup() {
 	const formik = useFormik({
@@ -21,17 +19,10 @@ function Signup() {
 			password: "",
 			passwordConfirm: "",
 		},
-		validationSchema,
+		validationScheme,
+		// TODO: validationScheme çalışmıyor?
 		onSubmit: async (values, bag) => {
-			try {
-				const registerResponse = await fetchRegister({
-					email: values.email,
-					password: values.password,
-				});
-				console.log(registerResponse);
-			} catch (error) {
-				bag.setErrors({ general: error.response.data.message });
-			}
+			console.log(values);
 		},
 	});
 	return (
@@ -40,11 +31,6 @@ function Signup() {
 				<Box pt={10}>
 					<Box textAlign={"center"}>
 						<Heading>Sign Up</Heading>
-					</Box>
-					<Box my={5}>
-						{formik.errors.general && (
-							<Alert status="error">{formik.errors.general}</Alert>
-						)}
 					</Box>
 					<Box my={5} textAlign={"left"}>
 						<form onSubmit={formik.handleSubmit}>
@@ -55,7 +41,6 @@ function Signup() {
 									onChange={formik.handleChange}
 									onBlur={formik.onBlur}
 									value={formik.values.email}
-									isInvalid={formik.touched.email && formik.errors.email}
 								/>
 							</FormControl>
 							<FormControl mt={4}>
@@ -66,7 +51,6 @@ function Signup() {
 									onChange={formik.handleChange}
 									onBlur={formik.onBlur}
 									value={formik.values.password}
-									isInvalid={formik.touched.password && formik.errors.password}
 								/>
 							</FormControl>
 							<FormControl mt={4}>
@@ -77,10 +61,6 @@ function Signup() {
 									onChange={formik.handleChange}
 									onBlur={formik.onBlur}
 									value={formik.values.passwordConfirm}
-									isInvalid={
-										formik.touched.passwordConfirm &&
-										formik.errors.passwordConfirm
-									}
 								/>
 							</FormControl>
 							<Button mt={4} width={"full"} type={"submit"}>
